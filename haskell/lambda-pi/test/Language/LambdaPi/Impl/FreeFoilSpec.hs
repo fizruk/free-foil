87f6b0423dde2e283afb49dd51991b666a75bb6b
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-06-19 15:15:49 +0300

    Test non-alpha-equivalent terms as well (free foil)

--- haskell/lambda-pi/test/Language/LambdaPi/Impl/FreeFoilSpec.hs
+++ haskell/lambda-pi/test/Language/LambdaPi/Impl/FreeFoilSpec.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Language.LambdaPi.Impl.FreeFoilSpec where
 
+import Control.Monad.State
 import Test.Hspec
 import Test.QuickCheck
 import Data.Bifunctor
+import Data.Bitraversable
 
 import qualified Control.Monad.Foil.Internal as Foil.Internal
 import Control.Monad.Foil
@@ -45,20 +49,52 @@ genAlphaEquivLambdaPis scope1 scope2 names = go
               return (Lam binder1 body1, Lam binder2 body2)
       ]
 
+-- | Alter at most @n@ names in a given expression.
+-- The result contains the number of unused changes and a new expression.
+-- If the result contains the original number, then no changes took place.
+alterNames :: Distinct n => Scope n -> [Name n] -> LambdaPi n -> StateT Int Gen (LambdaPi n)
+alterNames scope names = go
+  where
+    go = \case
+      t@(Var x) -> do
+        n <- get
+        (m, t') <- lift $ elements $
+          (n, t) : if n > 0 then [ (n - 1, Var name) | name <- names, name /= x ] else []
+        put m
+        return t'
+      Node node -> Node <$> bitraverse goScoped go node
+
+    goScoped (ScopedAST binder body) =
+      case (assertExt binder, assertDistinct binder) of
+        (Ext, Distinct) -> ScopedAST binder <$>
+          alterNames (extendScope binder scope) (nameOf binder : map sink names) body
+
 instance Arbitrary (LambdaPi VoidS) where
   arbitrary = genLambdaPi emptyScope []
 
-data AlphaEquivRefreshed = AlphaEquivRefreshed (LambdaPi VoidS) (LambdaPi VoidS)
+data AlphaEquiv = AlphaEquiv Bool (LambdaPi VoidS) (LambdaPi VoidS)
 
-instance Arbitrary AlphaEquivRefreshed where
-  arbitrary = do
-    (t, t') <- genAlphaEquivLambdaPis emptyScope emptyScope []
-    return (AlphaEquivRefreshed t t')
-
-instance Show AlphaEquivRefreshed where
-  show (AlphaEquivRefreshed t1 t2) = unlines
+instance Show AlphaEquiv where
+  show (AlphaEquiv equiv t1 t2) = unlines
     [ "t1 = " <> show t1
     , "t2 = " <> show t2
+    , if equiv
+        then "t1 and t2 are α-equivalent"
+        else "t1 and t2 are not α-equivalent"
+    ]
+
+instance Arbitrary AlphaEquiv where
+  arbitrary = do
+    (t, t') <- genAlphaEquivLambdaPis emptyScope emptyScope []
+    (alt, n) <- runStateT (alterNames emptyScope [] t) 1
+    return (AlphaEquiv (n == 1) alt t')
+
+newtype AlphaEquivRefreshed = AlphaEquivRefreshed AlphaEquiv
+  deriving (Arbitrary)
+
+instance Show AlphaEquivRefreshed where
+  show (AlphaEquivRefreshed ae@(AlphaEquiv _equiv t1 t2)) = unlines
+    [ show ae
     , "refreshAST _ t1 = " <> show (refreshAST emptyScope t1)
     , "refreshAST _ t2 = " <> show (refreshAST emptyScope t2)
     , "alphaEquivRefreshed _ t1 t2 = " <> show (alphaEquivRefreshed emptyScope t1 t2)
@@ -69,5 +105,5 @@ spec = do
   describe "α-equivalence" $ do
     it "alphaEquiv is correct" $ property $ forAll (genAlphaEquivLambdaPis emptyScope emptyScope []) $ \(t, t') ->
       alphaEquiv emptyScope t t'
-    it "alphaEquivRefreshed is correct" $ property $ \(AlphaEquivRefreshed t t') ->
-      alphaEquivRefreshed emptyScope t t'
+    it "alphaEquivRefreshed is correct" $ property $ \(AlphaEquivRefreshed (AlphaEquiv equiv t t')) ->
+      alphaEquivRefreshed emptyScope t t' `shouldBe` equiv
