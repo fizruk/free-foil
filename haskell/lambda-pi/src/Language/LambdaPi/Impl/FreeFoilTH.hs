533acbce741998b9d2030747c10fc98b1bcb6704
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-06-20 03:04:03 +0300

    Add Show and IsString instance in FreeFoilTH

--- haskell/lambda-pi/src/Language/LambdaPi/Impl/FreeFoilTH.hs
+++ haskell/lambda-pi/src/Language/LambdaPi/Impl/FreeFoilTH.hs
@@ -1,16 +1,28 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures    #-}
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE PatternSynonyms   #-}
 {-# LANGUAGE TemplateHaskell   #-}
 module Language.LambdaPi.Impl.FreeFoilTH where
 
+import Data.String (IsString(..))
 import qualified Control.Monad.Foil           as Foil
 import           Control.Monad.Free.Foil
 import           Control.Monad.Free.Foil.TH
 import           Data.Bifunctor.TH
 import           Data.Map                     (Map)
+import qualified Data.Map as Map
 import qualified Language.LambdaPi.Syntax.Abs as Raw
+import qualified Language.LambdaPi.Syntax.Par as Raw
+import qualified Language.LambdaPi.Syntax.Lex as Raw
+import qualified Language.LambdaPi.Syntax.Print as Raw
+
+-- $setup
+-- >>> :set -XOverloadedStrings
+-- >>> :set -XDataKinds
+-- >>> import qualified Control.Monad.Foil as Foil
 
 -- * Generated code
 
@@ -23,17 +35,26 @@ deriveBitraversable ''Term'Sig
 -- ** Pattern synonyms
 mkPatternSynonyms ''Term'Sig
 
--- ** Conversion
+-- ** Conversion helpers
 
 mkConvertToFreeFoil ''Raw.Term' ''Raw.VarIdent ''Raw.ScopedTerm' ''Raw.Pattern'
 mkConvertFromFreeFoil ''Raw.Term' ''Raw.VarIdent ''Raw.ScopedTerm' ''Raw.Pattern'
 
 -- * User-defined code
 
--- | Convert 'Raw.Term'' into a scope-safe representation.
+type Term = AST (Term'Sig Raw.BNFC'Position)
+
+-- ** Conversion helpers
+
+-- | Convert 'Raw.Term'' into a scope-safe term.
 -- This is a special case of 'convertToAST'.
 toTerm' :: Foil.Distinct n => Foil.Scope n -> Map Raw.VarIdent (Foil.Name n) -> Raw.Term' a -> AST (Term'Sig a) n
 toTerm' = convertToAST convertToTerm'Sig getPattern'Binder getTerm'FromScopedTerm'
+
+-- | Convert 'Raw.Term'' into a closed scope-safe term.
+-- This is a special case of 'toTerm''.
+toTerm'Closed :: Raw.Term' a -> AST (Term'Sig a) Foil.VoidS
+toTerm'Closed = toTerm' Foil.emptyScope Map.empty
 
 -- | Convert a scope-safe representation back into 'Raw.Term''.
 -- This is a special case of 'convertFromAST'.
@@ -48,3 +69,16 @@ fromTerm' = convertFromAST
   (Raw.PatternVar (error "location missing"))
   (Raw.AScopedTerm (error "location missing"))
   (\n -> Raw.VarIdent ("x" ++ show n))
+
+-- | Parse scope-safe terms via raw representation.
+--
+-- >>> fromString "λx.λy.λx.x" :: Term Foil.VoidS
+-- λ x0 . λ x1 . λ x2 . x2
+instance IsString (AST (Term'Sig Raw.BNFC'Position) Foil.VoidS) where
+  fromString input = case Raw.pTerm (Raw.tokens input) of
+    Left err -> error ("could not parse λΠ-term: " <> input <> "\n  " <> err)
+    Right term -> toTerm'Closed term
+
+-- | Pretty-print scope-safe terms via raw representation.
+instance Show (AST (Term'Sig a) Foil.VoidS) where
+  show = Raw.printTree . fromTerm'
